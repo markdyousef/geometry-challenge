0c0476f7a720937b0b92edf5f0bce1b11d51b583
markdyousef <markdyousef@gmail.com>
2018-08-13 07:26:17 +0200

    simple cmd interface and readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# Triangle-Challenge
+
+## Usage
+
+### Commandline (Node.js)
+
+_Installation:_
+
+**Manual**
+
+1. Make sure you have Node.js installed
+
+2. Install required node modules (run from project root directory)
+
+   npm install
+
+_Run:_
+Run node application with commandline arguments.
+A triangle with the sides `a = 1, b = 2, c = 3` for example:
+
+    node index.js 1 2 3 -> scalene
--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const { compareSides } = require("./lib/triangle-utils");
 
-const sides = process.argv.slice(2).map(arg => Number(arg));
-console.log(compareSides(...sides));
+function main() {
+  const sides = process.argv.slice(2).map(arg => Number(arg));
+  try {
+    const inputs = sides.map(inp => Number(inp));
+    const triangleType = compareSides(...inputs);
+    switch (triangleType) {
+      case 3:
+        return `${inputs} => equilateral`;
+      case 2:
+        return `${inputs} => isosceles`;
+      case 0:
+        return `${inputs} => scalene`;
+      default:
+        return null;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(main());
